f49b914d19c42021069d71eddc2c12ca4b3b118b
fabian-gl <64825031+fabian-gl@users.noreply.github.com>
2021-07-24 19:07:29 -0300

    Added db connection

---
+++ config/db.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql')
+
+let dbConnection = null
+
+const initDb = () => {
+    return new Promise ((resolve, reject) => {
+        if (dbConnection)
+        {
+            console.log('Database already connected')
+            resolve()
+        }
+        dbConnection = mysql.createConnection({
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME
+        })
+        dbConnection.connect(error => {
+            if (error)
+            {
+                dbConnection = null
+                reject(error)
+            }
+            resolve()
+        })
+    })
+}
+
+const getDbConnection = () => {
+    if (dbConnection) return dbConnection
+    console.log('The connection to the db is not open yet, call initDb() first')
+}
+
+module.exports = {
+    initDb,
+    getDbConnection
+}
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-
+const { initDb } = require('./config/db')
 
 dotenv.config({path: './config/config.env'})
+
+initDb().then(() => {
+    console.log('Connected to mySql!')
+}).catch(err => {
+    console.log(`Couldn't connect to database: ${err}`)
+})
+
 
 const app = express()
 const transactions = require('./routes/transactions')
 
 app.use('/api/v1/transactions', transactions)
 
-const PORT = process.env.PORT || 5000
+const SERVER_PORT = process.env.SERVER_PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode at port ${PORT}.`))
+app.listen(SERVER_PORT, console.log(`Server running in ${process.env.NODE_ENV} mode at port ${SERVER_PORT}.`))
