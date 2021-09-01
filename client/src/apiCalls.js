6a55759c936f01b07137d7fe656bebf33ffaff53
fabian-gl <64825031+fabian-gl@users.noreply.github.com>
2021-09-01 00:54:52 -0300

    Extracted out request header creation to avoid repetition.

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -2,42 +2,51 @@ import axios from 'axios'
 
 const apiRootUrl = 'http://localhost:5000'
 
-const getToken = () => localStorage.getItem('access_token') || ''
+
+const getAuthHeaders = () => {
+    const accessToken = localStorage.getItem('access_token') || ''
+    return { Authorization: `Bearer ${accessToken}` }
+}
 
 export const addTransaction = (name, amount, type, date) => {
-    return axios.post(`${apiRootUrl}/api/v1/transactions`,{name, amount, type, date},
-        { headers: {Authorization: `Bearer ${getToken()}`}} )
+    return axios.post(`${apiRootUrl}/api/v1/transactions`,
+        {name, amount, type, date}, 
+        { headers: getAuthHeaders()} )
 }
 
 export const updateTransaction = (id, name, amount, date) => {
-    return axios.put(`${apiRootUrl}/api/v1/transactions`, { id, name, amount, date },
-        { headers: {Authorization: `Bearer ${getToken()}`}} )
+    return axios.put(`${apiRootUrl}/api/v1/transactions`, 
+        { id, name, amount, date }, 
+        { headers: getAuthHeaders()} )
 }
 
 export const deleteTransaction = id => {
-    return axios.delete(`${apiRootUrl}/api/v1/transactions`, { data: { id }, 
-        headers: {Authorization: `Bearer ${getToken()}`} })
+    return axios.delete(`${apiRootUrl}/api/v1/transactions`, 
+        { data: { id }, 
+        headers: getAuthHeaders() })
 }
 
 export const getSummaryInformation = () => {
     return axios.get(`${apiRootUrl}/api/v1/transactions/summary`, 
-        { headers: {Authorization: `Bearer ${getToken()}`}} )
+        { headers: getAuthHeaders()} )
 }
 
 export const getTransactions = () => {
-    return axios.get(`${apiRootUrl}/api/v1/transactions`,
-        { headers: {Authorization: `Bearer ${getToken()}`}} )
+    return axios.get(`${apiRootUrl}/api/v1/transactions`, 
+        { headers: getAuthHeaders()} )
 }
 
 export const userLogin = userData => {
-    return axios.post(`${apiRootUrl}/api/v1/user/login`, {...userData })
+    return axios.post(`${apiRootUrl}/api/v1/user/login`, 
+        {...userData })
 }
 
 export const userRegister = userData => {
-    return axios.post(`${apiRootUrl}/api/v1/user/register`, {...userData})
+    return axios.post(`${apiRootUrl}/api/v1/user/register`, 
+        {...userData})
 }
 
 export const getUserName = () => {
-    return axios.get(`${apiRootUrl}/api/v1/user/name`,
-        { headers: {Authorization: `Bearer ${getToken()}`}} )
+    return axios.get(`${apiRootUrl}/api/v1/user/name`, 
+        { headers: getAuthHeaders()} )
 }
